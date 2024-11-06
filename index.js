50fc5ce58342ba8273e0e0a91580e81f9c11c556
rokkc <yashpattekar@gmail.com>
2024-11-06 09:12:58 -0800

    added doktor responses

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ closeLeftbar.addEventListener('click', function() {
     leftContainer.classList.toggle("squished");
 });
 
+const chatArea = document.getElementById("chatArea");
+function addAIResponse(text) {
+    chatArea.innerHTML += `
+    <div class="aiResponseContainer">
+        <div class="aiPfpContainer">
+            <img class=aiPfp src="images/doctorPfp.png">
+        </div>
+        <div class="aiTextContainer">
+            ${text}
+        </div>
+    </div>
+
+    `
+}
+
+for (let i = 0; i < 3; i++) {
+    addAIResponse("Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? ");
+}
