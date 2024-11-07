a6d6dbd71b1af9336b6ce36a9532dcc12dec7762
rokkc <yashpattekar@gmail.com>
2024-11-07 15:39:35 -0800

    added user response functionality

--- index.js
+++ index.js
@@ -7,18 +7,36 @@ closeLeftbar.addEventListener('click', function() {
 const chatArea = document.getElementById("chatArea");
 function addAIResponse(text) {
     chatArea.innerHTML += `
-    <div class="aiResponseContainer">
-        <div class="aiPfpContainer">
-            <img class=aiPfp src="images/doctorPfp.png">
+        <div class="aiResponseContainer">
+            <div class="aiPfpContainer">
+                <img class=aiPfp src="images/doctorPfp.png">
+            </div>
+            <div class="aiTextContainer">
+                ${text}
+            </div>
         </div>
-        <div class="aiTextContainer">
-            ${text}
-        </div>
-    </div>
 
     `
 }
 
-for (let i = 0; i < 3; i++) {
-    addAIResponse("Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? Hello! How can I help you today? ");
+function addUserResponse(text) {
+    chatArea.innerHTML += `
+        <div class="userResponseContainer">
+            <div class="userTextContainer">
+                <div class="userText">
+                    ${text}
+                </div>
+            </div>
+            <div class="userPfpContainer">
+                <div class="userPfp">
+                    You
+                </div>
+            </div>
+        </div>
+
+    `
+}
+
+for (let i = 0; i < 50; i++) {
+    addUserResponse("I have a headache")
 }
