c711217d2652d3002fa455ce0bbcf30630f6d841
Aarush Parikh <69699212+Humbalumba@users.noreply.github.com>
2024-11-16 16:10:58 -0800

    Delete static/js/env.js

--- static/js/env.js
+++
@@ -1,3 +0,0 @@
-const OPENAI_API_KEY = "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";
-
-export { OPENAI_API_KEY };
