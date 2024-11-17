18a5ca3fee5061abf233dc59fc15e1e23e9b79eb
Aarush Parikh <69699212+Humbalumba@users.noreply.github.com>
2024-11-17 11:57:59 -0800

    Update app.py

--- app.py
+++ app.py
@@ -236,6 +236,6 @@ def diagnose():
     else:
         return jsonify(user_message)
 if __name__ == '__main__':
-    threading.Thread(target=load_model, daemon=True).start()
+    load_model()
     port = int(os.environ.get("PORT", 5000))  # Get the port from Heroku or default to 5000
     app.run(host="0.0.0.0", port=port, debug=True)  # Bind to 0.0.0.0 to make it accessible externally
